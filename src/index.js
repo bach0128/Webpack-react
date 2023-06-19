eede22f9b108674fdecf2bc9e6c647765fd565cd
bach0128 <125292149+bach0128@users.noreply.github.com>
2023-06-19 14:56:09 +0700

    222

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import React from 'react' // nạp thư viện react
 import ReactDOM from 'react-dom' // nạp thư viện react-dom
 
+import { createRoot } from 'react-dom/client' //
+
 // Tạo component App
 function App() {
     return (
         <div>
             <h1>Xin chào anh em F8!</h1>
+            <h2>Hu Hi ho HtmlWebpackPlugin</h2>
         </div>
     )
 }
 
 // Render component App vào #root element
-ReactDOM.render(<App />, document.getElementById('root'))
+// ReactDOM.render(<App />, document.getElementById('root'))
+
+const root = createRoot(document.getElementById('root'))
+root.render(<App />)
 
